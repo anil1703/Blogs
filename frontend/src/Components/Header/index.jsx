fb646086ca2516752f5b66ffe22dd0244c8f980b
Thota Anil Sai Kumar <anilcherry1714@gmail.com>
2024-08-04 19:20:35 +0530

    FrontEnd

---
+++ frontend/src/Components/Header/index.jsx
@@ -0,0 +1,16 @@
+import "./index.css"
+
+const Header = () => {
+    render(){
+        return (
+            <nav>
+                <h1>Header</h1>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </nav>)
+    }
+}
+
+export default Header
